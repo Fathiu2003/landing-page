a37c43ada55b517228a9d7798780c8aa74b14973
Bello Fathiu <160033183+Fathiu2003@users.noreply.github.com>
2025-11-12 19:22:05 +0100

    Add form submission alert in Script.js

---
+++ Script.js
@@ -0,0 +1,5 @@
+// Simple placeholder: you can expand functionality later
+document.querySelector('form').addEventListener('submit', e=>{
+  e.preventDefault();
+  alert('Message sent! (demo)');
+});
